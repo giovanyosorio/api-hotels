77b54bcba403a577706dac494742b6d14fc418d6
giovany osorio <giovany.torres.799@gmail.com>
2023-09-19 17:02:06 -0500

    api de clothes

--- main.js
+++ main.js
@@ -1,24 +1,46 @@
-const url_images = 'https://booking-com.p.rapidapi.com/v1/metadata/exchange-rates?currency=AED&locale=en-gb';
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '5fbf1d95e5msh3f546b1f0bf5c35p16abfcjsna329b33ba65e',
-		'X-RapidAPI-Host': 'booking-com.p.rapidapi.com'
-	}
-};
+const url = 'https://fakestoreapi.com/products';
 
+const img1=document.getElementById("image1")
+const img2=document.getElementById("image2")
+const img3=document.getElementById("image3")
+const spanError = document.getElementById('error')
 
 async function fetchData(){
     try {
         
-        const response = await fetch(url, options);
+        const response = await fetch(url);
         const result = await response.json();
-        
-        console.log(result);
+        function shuffleArray(array) {
+            for (let i = array.length - 1; i > 0; i--) {
+                const j = Math.floor(Math.random() * (i + 1));
+                [array[i], array[j]] = [array[j], array[i]]; // Intercambia elementos
+            }
+            return array;
+        }
+        const randomFlags = shuffleArray([...result]).slice(0, 3);
+    
+        console.log(randomFlags);
+        if (response.status !== 200) {
+            spanError.innerHTML = "Hubo un error: " + res.status;
+        } 
+        image1.src=randomFlags[0].image
+        image2.src=randomFlags[1].image
+        image3.src=randomFlags[2].image
     } catch (error) {
         console.error(error);
     }
 }
-
+async function loadFavorites() {
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log('Favoritos')
+    console.log(data)
+  
+    if (res.status !== 200) {
+      spanError.innerHTML = "Hubo un error: " + res.status + data.message;
+    }
+  }
+  
 fetchData()
 
+loadFavorites()
